fb58e323ef7fd5a8e2c65edf912a15a42e2d2f09
Mihai Apostol <mihaiapostol153@gmail.com>
2025-05-28 15:28:23 +0300

    added new classes DriverHelper ElementChecker

--- main_parser.py
+++ main_parser.py
@@ -4,7 +4,11 @@ from selenium.webdriver.firefox.service import Service
 from selenium.common.exceptions import NoSuchElementException
 
 from config import USER_AGENT
-from helper import Helper
+from helper import (
+    Helper,
+    ElementChecker,
+    DriverHelper
+)
 
 
 class MainParser(Helper):
@@ -26,90 +30,27 @@ class MainParser(Helper):
         self.stop_page = stop_page
         self.user_requested_image = '+'.join(user_requested_image.split(' '))
 
+        self.checker = ElementChecker(driver=self.driver)
+        self.driver_helper = DriverHelper(driver=self.driver)
+
         self.get_item_link()
 
-    def xpath_exists(self, xpath):
-        """Checks if an element with the given XPath exists on the page.
-
-        Args:
-            xpath (str): The XPath of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by XPath
-            self.driver.find_element(By.XPATH, xpath)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def id_exists(self, element_id):
-        """Checks if an element with the given ID exists on the page.
-
-        Args:
-            element_id (str): The ID of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by XPath
-            self.driver.find_element(By.ID, element_id)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def class_exists(self, class_name):
-        """Checks if an element with the given class name exists on the page.
-
-        Args:
-            class_name (str): The class name of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by class name
-            self.driver.find_element(By.CLASS_NAME, class_name)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def send_by_url(self, url):
-        """
-        Navigates to the specified URL using the web driver.
-
-        Args:
-            url (str): The URL to navigate to.
-
-        Raises:
-            WebDriverException: If there is an issue with navigating to the URL.
-        """
-        # Use the web driver to open the specified URL
-        self.driver.get(url=url)
-
     def get_item_link(self):
-        image_links = None
-        for i in range(self.start_page, self.stop_page+1):
+
+        for i in range(self.start_page, self.stop_page + 1):
             self.random_pause_code(start=1, stop=4)
-            self.send_by_url(url=f'https://www.goodfon.ru/search/?q={self.user_requested_image}&page={i}')
+            self.driver_helper.send_by_url(url=f'https://www.goodfon.ru/search/?q={self.user_requested_image}&page={i}')
             self.random_pause_code(start=1, stop=4)
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        if self.class_exists(class_name='wallpapers'):
+        if self.checker.class_exists(class_name='wallpapers'):
 
             self.user_requested_image = self.user_requested_image.replace('+', '_')
 
             try:
                 self.create_directory(name_directory=self.user_requested_image)
-                image_page_links = [img.get_attribute('href') for img in self.driver.find_elements(By.TAG_NAME, 'a')  if 'wallpaper' in img.get_attribute('href')]
+                image_page_links = [img.get_attribute('href') for img in self.driver.find_elements(By.TAG_NAME, 'a') if
+                                    'wallpaper' in img.get_attribute('href')]
                 self.create_file_from_list(
                     filename=f"{self.user_requested_image}/{self.user_requested_image}_image_page_links.txt",
                     data_list=image_page_links
@@ -118,18 +59,7 @@ class MainParser(Helper):
             except NoSuchElementException:
                 ...
         else:
-            self.close_driver()
-
-
-
-    def close_driver(self):
-        """Close the WebDriver instance and quits the browser.
-
-        This method closes the current window and terminates the WebDriver session.
-        """
-        self.driver.close()  # Close the current window
-        self.driver.quit()  # Quit the WebDriver session and close all associated windows
-
+            self.driver_helper.close_driver()
 
 def main():
     return MainParser(
--- parser_image.py
+++ parser_image.py
@@ -4,11 +4,15 @@ from selenium.webdriver.firefox.service import Service
 from selenium.common.exceptions import NoSuchElementException
 
 from config import USER_AGENT
-from helper import Helper
+from helper import (
+    Helper,
+    ElementChecker,
+    DriverHelper
+)
 
 
 class ParserItemInfo(Helper):
-    def __init__(self,source_file=''):
+    def __init__(self, source_file=''):
         # Initialize Firefox options
         self.options = webdriver.FirefoxOptions()
         self.options.set_preference("general.useragent.override",
@@ -23,104 +27,18 @@ class ParserItemInfo(Helper):
                                         options=self.options)  # Create a new instance of the Firefox WebDriver with the specified options
 
         self.source_file = source_file
+        self.checker = ElementChecker(driver=self.driver)
+        self.driver_helper = DriverHelper(driver=self.driver)
 
         self.iter_by_item()
-
-    def xpath_exists(self, xpath):
-        """Checks if an element with the given XPath exists on the page.
-
-        Args:
-            xpath (str): The XPath of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by XPath
-            self.driver.find_element(By.XPATH, xpath)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def id_exists(self, element_id):
-        """Checks if an element with the given ID exists on the page.
-
-        Args:
-            element_id (str): The ID of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by XPath
-            self.driver.find_element(By.ID, element_id)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def class_exists(self, class_name):
-        """Checks if an element with the given class name exists on the page.
-
-        Args:
-            class_name (str): The class name of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by class name
-            self.driver.find_element(By.CLASS_NAME, class_name)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def tag_exists(self, tag_name):
-        """Checks if an element with the given class name exists on the page.
-
-        Args:
-            tag_name (str): The tag name of the element to check.
-
-        Returns:
-            bool: True if the element exists, False otherwise.
-        """
-        try:
-            # Attempt to find the element by tag name
-            self.driver.find_element(By.TAG_NAME, tag_name)
-            exist = True
-        except NoSuchElementException:
-            # If NoSuchElementException is raised, the element does not exist
-            exist = False
-        return exist
-
-    def send_by_url(self, url):
-        """
-        Navigates to the specified URL using the web driver.
-
-        Args:
-            url (str): The URL to navigate to.
-
-        Raises:
-            WebDriverException: If there is an issue with navigating to the URL.
-        """
-        # Use the web driver to open the specified URL
-        self.driver.get(url=url)
-
-
 
     def iter_by_item(self):
         with open(file=self.source_file, mode='r') as file:
             source = file.read()
             for url in source.split():
                 self.random_pause_code(start=1, stop=4)
-                self.send_by_url(url=url)
+                self.driver_helper.send_by_url(url=url)
                 self.get_image()
-
 
     def get_image(self):
         self.random_pause_code(start=1, stop=4)
@@ -128,11 +46,10 @@ class ParserItemInfo(Helper):
         self.driver.execute_script("arguments[0].scrollIntoView();", self.driver.find_element(By.TAG_NAME, 'img'))
 
         try:
-            if self.tag_exists(tag_name='img'):
+            if self.checker.tag_exists(tag_name='img'):
                 self.driver.find_element(By.TAG_NAME, 'img').click()
                 self.random_pause_code(start=1, stop=2)
                 image_link = self.driver.find_element(By.TAG_NAME, 'img').get_attribute('src')
-
 
                 self.crate_file(
                     filename=f"{self.source_file.split('/')[0]}/{self.source_file.split('/')[0]}_image_link.txt",
@@ -141,15 +58,8 @@ class ParserItemInfo(Helper):
                 )
 
         except NoSuchElementException:
-            self.close_driver()
+            self.driver_helper.close_driver()
 
-    def close_driver(self):
-        """Close the WebDriver instance and quits the browser.
-
-        This method closes the current window and terminates the WebDriver session.
-        """
-        self.driver.close()  # Close the current window
-        self.driver.quit()  # Quit the WebDriver session and close all associated windows
 
 
 def main():
